5b8eada86035ed7c4b1633a54699b00bebf4068c
ceanbrjr <celiojunior0110@gmail.com>
2025-10-21 15:07:17 -0300

    correção catalogo

---
+++ src/lib/supabase.js
@@ -0,0 +1,270 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.authService = exports.categoryService = exports.productService = exports.supabase = void 0;
+// src/lib/supabase.ts
+var supabase_js_1 = require("@supabase/supabase-js");
+var supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+var supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+exports.supabase = (0, supabase_js_1.createClient)(supabaseUrl, supabaseAnonKey);
+// Funções de Produtos
+exports.productService = {
+    getAll: function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, data, error;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0: return [4 /*yield*/, exports.supabase
+                            .from('produtos')
+                            .select("\n        *,\n        categoria:categorias(*)\n      ")
+                            .order('created_at', { ascending: false })];
+                    case 1:
+                        _a = _b.sent(), data = _a.data, error = _a.error;
+                        if (error)
+                            throw error;
+                        return [2 /*return*/, data];
+                }
+            });
+        });
+    },
+    create: function (product) {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, data, error;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0: return [4 /*yield*/, exports.supabase
+                            .from('produtos')
+                            .insert([product])
+                            .select()
+                            .single()];
+                    case 1:
+                        _a = _b.sent(), data = _a.data, error = _a.error;
+                        if (error)
+                            throw error;
+                        return [2 /*return*/, data];
+                }
+            });
+        });
+    },
+    update: function (id, product) {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, data, error;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0: return [4 /*yield*/, exports.supabase
+                            .from('produtos')
+                            .update(product)
+                            .eq('id', id)
+                            .select()
+                            .single()];
+                    case 1:
+                        _a = _b.sent(), data = _a.data, error = _a.error;
+                        if (error)
+                            throw error;
+                        return [2 /*return*/, data];
+                }
+            });
+        });
+    },
+    delete: function (id) {
+        return __awaiter(this, void 0, void 0, function () {
+            var error;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, exports.supabase.from('produtos').delete().eq('id', id)];
+                    case 1:
+                        error = (_a.sent()).error;
+                        if (error)
+                            throw error;
+                        return [2 /*return*/];
+                }
+            });
+        });
+    },
+    uploadImage: function (file) {
+        return __awaiter(this, void 0, void 0, function () {
+            var fileExt, fileName, filePath, uploadError, data;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        fileExt = file.name.split('.').pop();
+                        fileName = "".concat(Math.random(), ".").concat(fileExt);
+                        filePath = "produtos/".concat(fileName);
+                        return [4 /*yield*/, exports.supabase.storage
+                                .from('imagens')
+                                .upload(filePath, file)];
+                    case 1:
+                        uploadError = (_a.sent()).error;
+                        if (uploadError)
+                            throw uploadError;
+                        data = exports.supabase.storage.from('imagens').getPublicUrl(filePath).data;
+                        return [2 /*return*/, data.publicUrl];
+                }
+            });
+        });
+    },
+};
+// Funções de Categorias
+exports.categoryService = {
+    getAll: function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, data, error;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0: return [4 /*yield*/, exports.supabase
+                            .from('categorias')
+                            .select('*')
+                            .order('nome')];
+                    case 1:
+                        _a = _b.sent(), data = _a.data, error = _a.error;
+                        if (error)
+                            throw error;
+                        return [2 /*return*/, data];
+                }
+            });
+        });
+    },
+    create: function (nome) {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, data, error;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0: return [4 /*yield*/, exports.supabase
+                            .from('categorias')
+                            .insert([{ nome: nome }])
+                            .select()
+                            .single()];
+                    case 1:
+                        _a = _b.sent(), data = _a.data, error = _a.error;
+                        if (error)
+                            throw error;
+                        return [2 /*return*/, data];
+                }
+            });
+        });
+    },
+    update: function (id, nome) {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, data, error;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0: return [4 /*yield*/, exports.supabase
+                            .from('categorias')
+                            .update({ nome: nome })
+                            .eq('id', id)
+                            .select()
+                            .single()];
+                    case 1:
+                        _a = _b.sent(), data = _a.data, error = _a.error;
+                        if (error)
+                            throw error;
+                        return [2 /*return*/, data];
+                }
+            });
+        });
+    },
+    delete: function (id) {
+        return __awaiter(this, void 0, void 0, function () {
+            var error;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, exports.supabase.from('categorias').delete().eq('id', id)];
+                    case 1:
+                        error = (_a.sent()).error;
+                        if (error)
+                            throw error;
+                        return [2 /*return*/];
+                }
+            });
+        });
+    },
+};
+// Autenticação
+exports.authService = {
+    signIn: function (email, password) {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, data, error;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0: return [4 /*yield*/, exports.supabase.auth.signInWithPassword({
+                            email: email,
+                            password: password,
+                        })];
+                    case 1:
+                        _a = _b.sent(), data = _a.data, error = _a.error;
+                        if (error)
+                            throw error;
+                        return [2 /*return*/, data];
+                }
+            });
+        });
+    },
+    signOut: function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var error;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, exports.supabase.auth.signOut()];
+                    case 1:
+                        error = (_a.sent()).error;
+                        if (error)
+                            throw error;
+                        return [2 /*return*/];
+                }
+            });
+        });
+    },
+    getSession: function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var data;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, exports.supabase.auth.getSession()];
+                    case 1:
+                        data = (_a.sent()).data;
+                        return [2 /*return*/, data.session];
+                }
+            });
+        });
+    },
+    onAuthStateChange: function (callback) {
+        return exports.supabase.auth.onAuthStateChange(function (_event, session) {
+            callback(session);
+        });
+    },
+};
